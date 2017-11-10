52aec73cce72ecd58edde665ce4d788141360027
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2017-11-10 19:29:50 +0300

    🦇:

--- app/components/home-page/index.vue
+++ app/components/home-page/index.vue
@@ -10,6 +10,14 @@
                 <a :href="link">{{link}}</a>
             </li>
         </ul>
+        <br/>
+        <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/319822638&amp;color=%23000000&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=true"
+        ></iframe>
     </div>
 </template>
 <style module="homePage" src="./index.scss"/>
